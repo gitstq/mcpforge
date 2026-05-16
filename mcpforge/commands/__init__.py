4abe878cfae7989feb0288892d7550c169d2922f
琦琦 <56579081+gitstq@users.noreply.github.com>
2026-05-16 13:19:22 +0800

    Add mcpforge/commands/__init__.py

---
+++ mcpforge/commands/__init__.py
@@ -0,0 +1,27 @@
+"""
+MCPForge Commands
+"""
+
+from . import install_cmd
+from . import uninstall_cmd
+from . import list_cmd
+from . import search_cmd
+from . import info_cmd
+from . import init_cmd
+from . import validate_cmd
+from . import run_cmd
+from . import config_cmd
+from . import update_cmd
+
+__all__ = [
+    "install_cmd",
+    "uninstall_cmd",
+    "list_cmd",
+    "search_cmd",
+    "info_cmd",
+    "init_cmd",
+    "validate_cmd",
+    "run_cmd",
+    "config_cmd",
+    "update_cmd",
+]
